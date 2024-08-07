0f1a5cecffacf759d6593df486341ed53675bdc8
LostArrows27 <thelastofus2isnuts@gmail.com>
2024-08-07 16:12:11 +0700

    feat(search-product): make search page with product lists UI

--- src/main/java/sun/intern/controller/SearchController.java
+++ src/main/java/sun/intern/controller/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import sun.intern.model.Product;
 import sun.intern.service.ProductService;
 
@@ -22,23 +23,27 @@ public class SearchController {
     }
 
     @GetMapping("/search-results")
-    public String searchPage(
+    public ModelAndView searchPage(
         @RequestParam(name = "key", required = false, defaultValue = "") String query,
-        @RequestParam(name = "page", required = false, defaultValue = "1") int page,
-        Model model
+        @RequestParam(name = "page", required = false, defaultValue = "1") int page
     ) {
+        ModelAndView modelAndView = new ModelAndView("screens/products/search");
+
         int size = 10; // Number of items per page
 
         Page<Product> productPage = productService.getProductsByName(query, page, size);
 
         Product[] productsNew = productPage.getContent().toArray(new Product[0]);
 
+        for(Product product : productsNew) {
+            System.out.print(product.getName() + " ");
+        }
 
-        model.addAttribute("query", query);
-        model.addAttribute("page", page);
-        model.addAttribute("totalPages", productPage.getTotalPages());
-        model.addAttribute("products", productsNew);
+        modelAndView.addObject("query", query);
+        modelAndView.addObject("page", page);
+        modelAndView.addObject("totalPages", productPage.getTotalPages());
+        modelAndView.addObject("products", productsNew);
 
-        return "screens/products/search";
+        return modelAndView;
     }
 }
